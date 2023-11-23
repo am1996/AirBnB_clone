e6a8b218e18f4a1da44804a476219ec65a125f2b
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2023-11-22 17:33:15 -0800

    go

--- console.py
+++ console.py
@@ -8,7 +8,7 @@ import sys
 import uuid
 
 from models.base_model import BaseModel
-from models import storage
+from models import db_storage.py
 from models.user import User
 from models.place import Place
 from models.state import State
